916a702a4baf7e73d5b3786256f081788ebfee6b
Tim Gee <writetotimgee@gmail.com>
2019-04-27 00:34:06 +0100

    Fix the tests

--- src/test/java/com/trickl/model/oanda/transaction/JsonSerializationTest.java
+++ src/test/java/com/trickl/model/oanda/transaction/JsonSerializationTest.java
@@ -1,19 +1,32 @@
 package com.trickl.model.oanda.transaction;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import static com.trickl.assertj.core.api.JsonObjectAssertions.assertThat;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class JsonSerializationTest {
 
+  private static ObjectMapper objectMapper;
+    
+  @BeforeAll
+  public static void setup() {
+    objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+  }          
+  
   @ParameterizedTest
   @MethodSource("pojoProvider")
   public void testSerialization(Object obj) throws Exception {  
     assertThat(obj)
+        .usingObjectMapper(objectMapper)
         .serializesAsExpected()
         .deserializesAsExpected()
-@JsonPropertyOrder({"id", "time", "userID", "accountID", "batchID", "requestID"})
         .schemaAsExpected();
   }
   
